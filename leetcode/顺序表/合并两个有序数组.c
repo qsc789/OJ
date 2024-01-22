3353655d96afcf0246820a2eb447b4ca8d4ff43d
qsc789 <1279495882@qq.com>
2024-01-22 20:34:06 +0800

    8

---
+++ leetcode/顺序表/合并两个有序数组.c
@@ -0,0 +1,36 @@
+//88. 合并两个有序数组
+//给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+//请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+//注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。 
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int end1 = m - 1;
+    int end2 = n - 1;
+    int end = m + n - 1;
+    while (end1 >= 0 && end2 >= 0)
+    {
+        if (nums1[end1] > nums2[end2])
+        {
+            nums1[end] = nums1[end1];
+            end--;
+            end1--;
+        }
+        else
+        {
+            nums1[end] = nums2[end2];
+            end--;
+            end2--;
+        }
+    }
+    while (end2 >= 0)
+    {
+        nums1[end] = nums2[end2];
+        end--;
+        end2--;
+    }
+
+    //end1>0不用处理
+
+}
