07cbe6dc8fac69b4b38040c6f2843e7d52f074bf
qsc789 <1279495882@qq.com>
2024-01-30 14:35:37 +0800

    递归轴对称

--- leetcode/二叉树/对称二叉树.c
+++ leetcode/二叉树/对称二叉树.c
@@ -1,5 +1,19 @@
 //力扣101
 //给你一个二叉树的根节点 root ， 检查它是否轴对称。
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ * };
+ */
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param pRoot TreeNode类
+ * @return TreeNode类
+ */
 bool Check(struct TreeNode* root1, struct TreeNode* root2)
 {
     if (root1 == NULL && root2 == NULL)
